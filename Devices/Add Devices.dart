c96de38df5b16d34547ef934db5368bb71584ff7
Warmisharrow <87402431+WarmisharrowPy@users.noreply.github.com>
2023-09-28 22:38:18 +0530

    Create Add Devices.dart

---
+++ Devices/Add Devices.dart
@@ -0,0 +1,156 @@
+import '/auth/firebase_auth/auth_util.dart';
+import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'add_devives_model.dart';
+export 'add_devives_model.dart';
+
+class AddDevivesWidget extends StatefulWidget {
+  const AddDevivesWidget({Key? key}) : super(key: key);
+
+  @override
+  _AddDevivesWidgetState createState() => _AddDevivesWidgetState();
+}
+
+class _AddDevivesWidgetState extends State<AddDevivesWidget> {
+  late AddDevivesModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => AddDevivesModel());
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: FlutterFlowTheme.of(context).secondaryBackground,
+      appBar: AppBar(
+        backgroundColor: FlutterFlowTheme.of(context).secondaryBackground,
+        automaticallyImplyLeading: false,
+        leading: InkWell(
+          splashColor: Colors.transparent,
+          focusColor: Colors.transparent,
+          hoverColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          onTap: () async {
+            context.pop();
+          },
+          child: Icon(
+            Icons.chevron_left_rounded,
+            color: FlutterFlowTheme.of(context).grayLight,
+            size: 32,
+          ),
+        ),
+        title: Text(
+          FFLocalizations.of(context).getText(
+            'q9vswkq7' /* Add Devices */,
+          ),
+          style: FlutterFlowTheme.of(context).headlineSmall,
+        ),
+        actions: [],
+        centerTitle: false,
+        elevation: 0,
+      ),
+      body: Container(
+        width: MediaQuery.sizeOf(context).width,
+        height: MediaQuery.sizeOf(context).height * 1,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.fitWidth,
+            image: Image.asset(
+              'assets/images/login_bg@2x.png',
+            ).image,
+          ),
+        ),
+        child: StreamBuilder<UsersRecord>(
+          stream: UsersRecord.getDocument(currentUserReference!),
+          builder: (context, snapshot) {
+            // Customize what your widget looks like when it's loading.
+            if (!snapshot.hasData) {
+              return Center(
+                child: SizedBox(
+                  width: 40,
+                  height: 40,
+                  child: SpinKitPumpingHeart(
+                    color: FlutterFlowTheme.of(context).primary,
+                    size: 40,
+                  ),
+                ),
+              );
+            }
+            final columnUsersRecord = snapshot.data!;
+            return Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Expanded(
+                        child: Text(
+                          FFLocalizations.of(context).getText(
+                            'u937ksgd' /* Step 1: First Connect your sma... */,
+                          ),
+                          style: FlutterFlowTheme.of(context).bodySmall,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
+                  child: FFButtonWidget(
+                    onPressed: () async {
+                      context.pushNamed('add');
+                    },
+                    text: FFLocalizations.of(context).getText(
+                      'xyxdikti' /* Add */,
+                    ),
+                    options: FFButtonOptions(
+                      width: 190,
+                      height: 50,
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                      iconPadding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                      color: FlutterFlowTheme.of(context).primary,
+                      textStyle:
+                          FlutterFlowTheme.of(context).titleSmall.override(
+                                fontFamily: 'Lexend',
+                                color: FlutterFlowTheme.of(context).textColor,
+                              ),
+                      elevation: 3,
+                      borderSide: BorderSide(
+                        color: Colors.transparent,
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
