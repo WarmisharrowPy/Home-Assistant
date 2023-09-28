d318ff9d1c5d6b40fd5d0aef73b28d4219896a0e
Warmisharrow <87402431+WarmisharrowPy@users.noreply.github.com>
2023-09-28 22:26:13 +0530

    Create Text.dart

---
+++ Login Page/Text.dart
@@ -0,0 +1,17 @@
+// Generated code for this Text Widget...
+Align(
+  alignment: AlignmentDirectional(0.00, 0.00),
+  child: Text(
+    FFLocalizations.of(context).getText(
+      'o07j3mgv' /* Welcome back */,
+    ),
+    style: FlutterFlowTheme.of(context).displaySmall,
+  ),
+)
+// Generated code for this Text Widget...
+Text(
+  FFLocalizations.of(context).getText(
+    'fzxvw3mu' /* Login to access your Account. */,
+  ),
+  style: FlutterFlowTheme.of(context).titleMedium,
+)
