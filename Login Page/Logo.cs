5265daf4811187a3e885efdf77298dabd7692d36
Warmisharrow <87402431+WarmisharrowPy@users.noreply.github.com>
2023-09-28 22:18:07 +0530

    Create Logo.cs

---
+++ Login Page/Logo.cs
@@ -0,0 +1,9 @@
+// Generated code for this Logo Widget...
+if (Theme.of(context).brightness == Brightness.dark)
+  Image.asset(
+    'assets/images/LOGO.jpg',
+    width: 340,
+    height: 90,
+    fit: BoxFit.fitWidth,
+    alignment: Alignment(0.00, 0.00),
+  )
